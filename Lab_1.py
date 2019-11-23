28b17f7d5b6339974762489ac1ea59b804318691
Jedama <dan.eastgrove@gmail.com>
2019-11-23 13:11:58 +0100

    Merge branch 'master' of /home/dan/PycharmProjects/DVA427 with conflicts.

--- Lab_1.py
+++ Lab_1.py
@@ -11,11 +11,13 @@ def sigmoid_derivative(x):
 size = 2
 amount = 3
 layers = 2
+
 rate = 5
 
 
 
 training_inputs = np.array([[1, 0], [1, 1], [0, 1], [1, 0]])
+
 target = np.array([[0, 0, 1, 0]]).T
 
 weight1 = 2 * np.random.random((size, size)) - 1
@@ -26,7 +28,7 @@ inputs = training_inputs
 s = (size, 3)
 layer1 = np.zeros(s)  # 0 = value, 1 = delta, 2 = w0i
 
-output = np.array([[0, 0]]).T
+output = np.zeros((2,1))
 
 for R in range(amount):
     for i in range(size):  # Setting W0i
@@ -34,19 +36,16 @@ for R in range(amount):
 
     for i in range(size):  # Calculating output for layer 1 nodes
 
-        layer1[i, R] += sigmoid(inputs[i, 0] * weight1[i, 0])
+        layer1[i, 0] += sigmoid(inputs[i, 0] * weight1[i, 0])
 
     for i in range(size):  # Calculating output for layer 2 nodes (total output in this case)
 
-        temp = sigmoid(layer1[i, 0] * weight2[i])
-        output[0] = temp
-        print(sigmoid(layer1[i, 0] * weight2[i]))
-        print(output)
+        output[0] = sigmoid(layer1[i, 0] * weight2[i])
 
-    output[1] = output[0] * (1 - output[0]) * (target[0] - output[0])  # Delta for output
+    output[1] = output[0] * (1 - output[0]) * (target[0] - output[0])  # Error for output
 
     for i in range(size):
-        layer1[i, 1] = layer1[i, 0] * (1 - layer1[i, 0]) * weight2[i] * output[1]  # Delta for layer1 i
+        layer1[i, 1] = layer1[i, 0] * (1 - layer1[i, 0]) * weight2[i] * output[1]  # Error for layer1 i
         layer1[i, 2] += rate * layer1[i, 1]  # W0i for Layer1 nodes
         weight2[i] += rate * output[1] * layer1[i, 0]  # New weights for layer2
 
@@ -54,4 +53,4 @@ for R in range(amount):
         for j in range(size):  # Layer1
             weight1[i, j] = rate * layer1[j, 1] * inputs[i, 0]  # New weigth1
 
-    #print(output[0])
+    print(output[0])
