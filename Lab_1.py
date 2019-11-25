27edd90644cd99d4d4764d2e46283d05d2c63a8e
msd16007@student.mdh.se <strunt91>
2019-11-25 14:48:07 +0100

    Testar av NN

--- Lab_1.py
+++ Lab_1.py
@@ -8,84 +8,91 @@ class NeuralNetwork():  # class for related functions
 
     # initialize the weigths
     def __init__(self):
+
         np.random.seed(1)
         self.synaptic_weights = 2 * np.random.random((19, 1)) - 1
 
     # commit lots of math
-    def sigmoid_nn(self, x):
+    def sigmoid(self, x):
+
         return 1 / (1 + np.exp(-x))
 
     def sigmoid_derivative(self, x):
-        return x(1 - x)
 
+        return x * (1 - x)
 
     def train(self, training_inputs, training_outputs, training_iterations):
-        for iteration in range(training_iterations):
+
+        for i in range(training_iterations):
+
             output = self.think(training_inputs)
             error = training_outputs - output
             weight_adjustments = np.dot(training_inputs.T, error * self.sigmoid_derivative(output))
             self.synaptic_weights += weight_adjustments
 
     def think(self, inputs):
+
         inputs = inputs.astype(float)
         output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
         return output
 
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+if __name__ == '__main__':  # typ våran main tror jag.
 
-if __name__ == '__main__': # typ våran main tror jag.
+    data_segmentation()  # Imports and sorts data
+    NN = NeuralNetwork()
 
-
-    data_segmentation()
 
     size = 18
     amount = 16 * 54
     layers = 2
-    rate = 0.1
+    learning_rate = 0.1
+    inputs = training_inputs
+    outputs = training_outputs
 
-    #training_inputs = np.array([[1, 0],
-    #                        [1, 1],
-    #                        [0, 1],
-    #                        [1, 0]])
-
-    #target = np.array([[0, 0, 1, 0]]).T
 
     weight1 = 2 * np.random.random((size, size)) - 1
     weight2 = 2 * np.random.random(size) - 1
+    layer1 = np.zeros((size, 3))  # 0 = value, 1 = delta, 2 = w0i
+    output = np.zeros((2, 1))
 
-    inputs = training_inputs
+    #Ser om jag får samma resultat med matrismultiplikation
+    if 0:
+        for R in range(1):
+            for i in range(size):  # Setting W0i
+                layer1[i, 2] = 2 * np.random.random_sample() - 1
 
-    s = (size, 3)
-    layer1 = np.zeros(s)  # 0 = value, 1 = delta, 2 = w0i
+            for i in range(size):  # Calculating output for layer 1 nodes
 
-    s2 = (2, 1)
-    output = np.zeros(s2)
+                layer1[i, 0] += NN.sigmoid(inputs[0, 0, i] * weight1[i, 0])
 
-    for R in range(1):
-        for i in range(size):  # Setting W0i
-            layer1[i, 2] = 2 * np.random.random_sample() - 1
+            for i in range(size):  # Calculating output for layer 2 nodes (total output in this case)
 
-        for i in range(size):  # Calculating output for layer 1 nodes
+                output[0] = NN.sigmoid(layer1[i, 0] * weight2[i])
 
-            layer1[i, 0] += sigmoid(inputs[0, 0, i] * weight1[i, 0])
+            output[1] = output[0] * (1 - output[0]) * (training_outputs[0, 0] - output[0])  # Delta for output
 
-        for i in range(size):  # Calculating output for layer 2 nodes (total output in this case)
+            for i in range(size):
+                layer1[i, 1] = layer1[i, 0] * (1 - layer1[i, 0]) * weight2[i] * output[1]  # Delta for layer1 i
+                layer1[i, 2] += learning_rate * layer1[i, 1]  # W0i for Layer1 nodes
+                weight2[i] += learning_rate * output[1] * layer1[i, 0]  # New weights for layer2
+                print(weight2[i])
 
-            output[0] = sigmoid(layer1[i, 0] * weight2[i])
+            for i in range(size):  # Inputs
+                for j in range(size):  # Layer1
+                    weight1[i, j] = learning_rate * layer1[j, 1] * inputs[0, 0, i]  # New weight1
 
-        output[1] = output[0] * (1 - output[0]) * (training_outputs[0, 0] - output[0])  # Delta for output
 
-        for i in range(size):
-            layer1[i, 1] = layer1[i, 0] * (1 - layer1[i, 0]) * weight2[i] * output[1]  # Delta for layer1 i
-            layer1[i, 2] += rate * layer1[i, 1]  # W0i for Layer1 nodes
-            weight2[i] += rate * output[1] * layer1[i, 0]  # New weights for layer2
-            print(weight2[i])
+#Mathias test
+print("synaptic weights before training: ", NN.synaptic_weights)
 
-        for i in range(size):  # Inputs
-            for j in range(size):  # Layer1
-                weight1[i, j] = rate * layer1[j, 1] * inputs[0, 0, i]  # New weight1
+NN.train(inputs[0],outputs[0],10000)
 
-    #print(output[0])
+print("Synaptic weights after training: ", NN.synaptic_weights)
+
+
+
+
+
+
--- Mathias_test.py
+++ Mathias_test.py
@@ -4,12 +4,12 @@ import random
 
 # Data transfer stuff, changed to 19 inputs as referenced by the pdf
 data_array = np.array(pd.read_csv("assignment1.txt"))  # Reads from file
-np.set_printoptions(formatter={'float': '{: 0.3f}'.format})  # formats to 3 decimal places
-#random.shuffle(data_array)  # randomises the data
+np.set_printoptions(formatter={'float': '{: 0.9f}'.format})  # formats to 3 decimal places
+#random.shuffle(data_array)  # randomises the data, unneccesary at first
 training_inputs = np.zeros((16, 54, 19))  # global variables for the data
 validation_data = np.zeros((115, 19))
 test_data = np.zeros((172, 19))
-training_outputs = np.zeros((16, 54))
+training_outputs = np.zeros((16, 54, 1))
 validation_result = np.zeros(115)
 test_result = np.zeros(172)
 
@@ -18,7 +18,7 @@ def data_segmentation():
     for i in range(16):  # transfers the training set
         for j in range(54):
             training_inputs[i, j, :] = data_array[j + i * 54, 0:19]
-            training_outputs[i, j] = data_array[j + i * 54, 19]
+            training_outputs[i, j,:] = data_array[j + i * 54, 19]
 
     for i in range(864, 979):  # transfers validation set
         validation_data[i - 864, :] = data_array[i, 0:19]
@@ -28,3 +28,5 @@ def data_segmentation():
         test_result[i - 979] = data_array[i, 19]
 
     print("\nData has been segmented\n")
+
+
