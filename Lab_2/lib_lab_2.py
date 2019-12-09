6464a1c765bb2fffdad2648420b31635c0bd2c3d
msd16007@student.mdh.se <strunt91>
2019-12-09 15:39:19 +0100

    Math functions!

--- Lab_2/lib_lab_2.py
+++ Lab_2/lib_lab_2.py
@@ -3,25 +3,31 @@ import numpy as np
 import pandas as pd
 
 data_array = np.array(pd.read_csv("iris.txt", header=None))  # Reads from file
-np.set_printoptions(formatter={'float': '{: 0.5f}'.format})  # formats to 5 decimal places
+np.set_printoptions(formatter={'float': '{: 0.2f}'.format})  # formats to 5 decimal places
 np.random.seed()
-random.shuffle(data_array)  # Shuffles data
+#random.shuffle(data_array)  # Shuffles data
 
 
 
 class data_management():
 
-    def __init__(self):
-        x=0 # variables
+    #def __init__(self):
+
     #def segmentation(self):
 
-    #def normalization(self):
+    def normalization(self):
+        for i in range(4):
+            xmax=max(data_array[:,i])
+            xmin=min(data_array[:,i])
+            data_array[:,i]=(data_array[:,i]-xmin)/(xmax-xmin)
 
     #def randomizing(self):
 
 
 
 DM=data_management()
+DM.normalization()
+
 
 
 
--- Lab_2/main.py
+++ Lab_2/main.py
@@ -1,4 +1,36 @@
 from lib_lab_2 import *
 
-print(data_array):)
-#
+
+# Math functions that returns how much it belongs to each classification
+def short(x):
+    if x<0.6:
+        return -5 * x / 3 + 1
+    else:
+        return 0
+
+def medium(x):
+    if x < 0.6:
+        return 5 * x / 3
+    else:
+        return -5 * x / 2 + 5 / 2
+
+
+def long(x):
+    if x>0.6:
+        return 5 * x / 2 - 1.5
+    else:
+        return 0
+
+def parametrize(x):
+
+    for i in range(4):
+        x[i]=
+
+
+if __name__ == '__main__':
+    print(data_array[0, 0:4])
+
+    print(short(data_array[0, 0:4]))
+    print(low_medium(data_array[0, 0:4]))
+    print(high_medium(data_array[0, 0:4]))
+    print(long(data_array[0, 0:4]))
