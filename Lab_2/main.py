16198a68e38cd4801c061ba31d55d4c45facf2f3
msd16007@student.mdh.se <strunt91>
2019-12-09 16:41:28 +0100

    See if this counts as a commit!

--- Lab_2/main.py
+++ Lab_2/main.py
@@ -1,7 +1,7 @@
 from lib_lab_2 import *
 
 
-# Math functions that returns how much it belongs to each classification
+# Math functions that returns how much each x_i belongs to each classification
 def short(x):
     if x<0.6:
         return -5 * x / 3 + 1
@@ -21,15 +21,14 @@ def long(x):
     else:
         return 0
 
-def parametrize(x):
+def compute_flower(x):  # takes all four parameters of each flower
+    x_temp = np.zeros((4,1))
 
-    for i in range(4):
-        print(i)
 
 
 if __name__ == '__main__':
 
-    """
+    """ Classify all lines according to these paramaters and then check which it is most of.
     versicolor1: 
     (x1 == short or long) and (x2 == medium or long) and (x3 == medium or long) and (x4 == short)
     
@@ -43,8 +42,4 @@ if __name__ == '__main__':
     (X2 == short or medium) and (x3 == long) (x4 == long)
     """
 
-    for i in range(4):
-        print(short(data_array[0, i]))
-        print(medium(data_array[0, i]))
-        print(long(data_array[0, i]))
-        print("\n")
+
