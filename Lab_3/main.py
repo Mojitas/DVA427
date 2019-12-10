86a3684d636920ebe8daeda7051086f83f4194bf
Jedama <dan.eastgrove@gmail.com>
2019-12-10 12:26:22 +0100

    :Y

--- Lab_3/main.py
+++ Lab_3/main.py
@@ -1,3 +1,25 @@
-print("hello world!") ####
+import numpy as np
+import random as rng
 
-x=1
+data_array = np.array(pd.read_csv("berlin52.tsp", header=None))
+
+amount = 20
+
+salesmen = np.zeros((amount, 53))
+
+for i in range(52):
+    salesmen[:, i] = i + 1
+
+salesmen[:, 52] = 1
+
+for i in range(amount):
+
+    rng.shuffle(salesmen[i])
+
+print(salesmen[0, :])
+print(salesmen[1, :])
+
+for j in range(amount):
+    for i in range(53):
+
+        data_array(salesmen(j, i),
