fb2e81cbfbb30357e23d445e9a8308746f1d28f9
Mojitas <strunt91>
2019-12-11 13:23:20 +0100

    Commented half of Lab 3
    Signed-off-by: Mojitas <strunt91>

--- Lab_3/main.py
+++ Lab_3/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import math
 
 ##
-amount = 100  # Thirty salesmen in each generation
+amount = 100  # salesmen in each generation
 
 data_array = np.array(pd.read_csv("berlin52.tsp", header=None))
 data_array = data_array.astype(int)
@@ -16,13 +16,13 @@ def init():  # starting conditions
     for i in range(52):
         salesmen[:, i] = i + 1  # set cities in order for all the salesmen
 
-    #salesmen[:, 52] = 1     # actually redundant but left until further inspection
-    #salesmen = salesmen.astype(int)
+    # salesmen[:, 52] = 1     # actually redundant but left until further inspection
+    # salesmen = salesmen.astype(int)
 
     for i in range(amount):
         rng.shuffle(salesmen[i, 1:52])  # shuffle order of the cities
 
-    salesmen[:, 52] = salesmen[:, 0] # start is same as end
+    salesmen[:, 52] = salesmen[:, 0]  # start is same as end
 
     return salesmen
 
@@ -30,7 +30,7 @@ def init():  # starting conditions
 def calculate(salesmen):  # distance between two places
     salesmen = salesmen.astype(int)
 
-    for j in range(amount): # for all salesmen
+    for j in range(amount):  # for all salesmen
 
         for i in range(52):
             x1 = data_array[salesmen[j, i] - 1, 1]
@@ -39,7 +39,7 @@ def calculate(salesmen):  # distance between two places
             x2 = data_array[salesmen[j, i + 1] - 1, 1]
             y2 = data_array[salesmen[j, i + 1] - 1, 2]  # pick coordinates of the next city
 
-            distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)   # calculates the distance between the two cities
+            distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)  # calculates the distance between the two cities
 
             salesmen[j, 53] += distance  # adds it to the total distance
 
@@ -47,25 +47,24 @@ def calculate(salesmen):  # distance between two places
 
 
 def elitism(salesmen):
-    elite = np.zeros((50, 54))   # Variable for choosing choosing who gets to live
+    elite = np.zeros((50, 54))  # Variable for choosing choosing who gets to live
     elite = elite.astype(int)
 
     for j in range(50):  # number of best ones to save
 
         tempmin = min(salesmen[:, 53])  # check for shortest path
 
-        for i in range(amount):     # Check through all the salesmen
+        for i in range(amount):  # Check through all the salesmen
 
             if tempmin == salesmen[i, 53]:  # If we got a match for good result
                 tempmini = i
 
-        elite[j] = salesmen[tempmini]   # Then transfer to the elites
+        elite[j] = salesmen[tempmini]  # Then transfer to the elites
 
-        salesmen[tempmini, 53] = 100000     # Set to big value to not find the same path again
-
-
+        salesmen[tempmini, 53] = 100000  # Set to big value to not find the same path again
 
     return elite
+
 
 def cross(parent1, parent2):
     child = np.zeros((1, 54))
@@ -74,10 +73,10 @@ def cross(parent1, parent2):
     remaining = np.zeros((1, 54))
     remaining = remaining.astype(int)
 
-    for j in range(10):     #Repeats ten times
+    for j in range(10):  # Repeats ten times
 
         random1 = rng.randint(0, 49)
-        randamount = rng.randint(1,10)
+        randamount = rng.randint(1, 10)
 
         for i in range(randamount):
             child[0, random1 + i] = parent1[0, random1 + i]
@@ -102,20 +101,21 @@ def cross(parent1, parent2):
 
     return child
 
+
 def cross2(parent1, parent2):
-    child = np.zeros((1, 54))
+    child = np.zeros((1, 54))  # new salesmen
     child = child.astype(int)
 
-    child[0, 0] = 1
+    child[0, 0] = 1  # starting city
 
-    remaining = np.zeros((1, 54))
+    remaining = np.zeros((1, 54))  #
     remaining = remaining.astype(int)
 
-    randamount = rng.randint(15, 20)
+    randamount = rng.randint(10, 15)        # 15-20 gener
     random1 = rng.randint(1, 52 - randamount)
 
-    for i in range(randamount):
-        child[0, random1 + i] = parent1[0, random1 + i]
+    for i in range(randamount):     # Get
+        child[0, random1 + i] = parent1[0, random1 + i]     # Get genes from parent
 
     empty = 0
 
@@ -139,8 +139,6 @@ def cross2(parent1, parent2):
 
 
 def crossover(elite):
-
-
     np.random.shuffle(elite)
 
     children = np.zeros((amount, 54))
@@ -189,22 +187,20 @@ def mutate(salesmen):
 
     return salesmen
 
-def mutate2(salesmen):
 
+def mutate2(salesmen):
     for j in range(amount):
 
-        mutations = rng.randint(1, 8)
+        mutations = rng.randint(1, 7)
 
         reverse = np.zeros((1, mutations))
 
         random1 = rng.randint(1, 52 - mutations)
 
         for i in range(mutations):
-
             reverse[0, mutations - 1 - i] = salesmen[j, random1 + i]
 
         for i in range(mutations):
-
             salesmen[j, random1 + i] = reverse[0, i]
 
     salesmen[:, 52] = salesmen[:, 0]
@@ -214,7 +210,7 @@ def mutate2(salesmen):
 
 if __name__ == '__main__':
 
-    iterations = 10000
+    iterations = 10000  # generations
 
     population = init()
 
@@ -231,7 +227,7 @@ if __name__ == '__main__':
 
         average2 += average
 
-        if R % 50 == 0:        # print every 100
+        if R % 50 == 0:  # print every 100
 
             if R != 0:
                 average2 = average2 / 50
@@ -240,11 +236,10 @@ if __name__ == '__main__':
 
             average2 = 0
 
-            #print("Longest path: ", max(population[:, 53]))
-            #print("Shortest path: ", min(population[:, 53]))
+            # print("Longest path: ", max(population[:, 53]))
+            # print("Shortest path: ", min(population[:, 53]))
 
         if R % 200 == 0:
-
             print("Shortest found path: ", lowest)
 
         elitepop = elitism(population)
@@ -252,7 +247,7 @@ if __name__ == '__main__':
 
         population = crossover(elitepop)
         population = mutate2(population)
-        #population = mutate(population)
+        # population = mutate(population)
 
         population[:, 53] = 0
 
