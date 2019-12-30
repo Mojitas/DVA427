788d01c36af5bfc25b530e73258671e19cd158ec
Jedama <dan.eastgrove@gmail.com>
2019-12-30 13:33:31 +0100

    :I

--- Lab_4/Dan_test.py
+++ Lab_4/Dan_test.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 
 data_array = np.array(pd.read_csv("city 1.txt", header=None), dtype=(np.unicode_, 1))
-"""
-path = [5, 15]  # This isn't used...
-node_list = [path for i in range(23)]  # init the list with 23 nodes of type path
-# print(node_list)
-"""
+
+Paths = np.zeros((23, 10))
+Paths[:, :] = -1
+Paths[5, 0] = 5
+
 
 class Graph:
 
@@ -25,19 +25,33 @@ class Graph:
         dist = [float("Inf")] * self.Nodes
         dist[src] = 0
 
-        for i in range(2):
+        for i in range(self.Nodes - 1):
             for u, v, w in self.graph:
                 if dist[u] != [float("Inf")] and dist[u] + w < dist[v]:
                     dist[v] = dist[u] + w
-                    print("Distance to " + str(v) + " has been updated via " + str(u))
+                    #print("Distance to " + str(v) + " has been updated via " + str(u))
+
+                    Paths[v, :] = 0
+                    Paths[v, :] = Paths[u, :]
+
+                    stop = 0
+                    j = 0
+
+                    while stop == 0:
+                        j = j + 1
+                        if Paths[v, j] == -1: stop = 1
+
+                    Paths[v, j] = v
 
         self.printArr(dist)
 
 
-G = Graph(23)   # init the graph with 23 nodes
-for i in range(35):     # Add the vertices(as numbers) and cost of edges
+G = Graph(23)  # init the graph with 23 nodes
+for i in range(35):  # Add the vertices(as numbers) and cost of edges
     G.addEdge(ord(data_array[i, 0]) - 65, ord(data_array[i, 1]) - 65, data_array[i, 2].astype(int))
-for i in range(35):     # Undirected graph, each edge goes both ways
+for i in range(35):  # Undirected graph, each edge goes both ways
     G.addEdge(ord(data_array[i, 1]) - 65, ord(data_array[i, 0]) - 65, data_array[i, 2].astype(int))
 
-G.BellmanFord(5)    # Find 5, aka F
+G.BellmanFord(5)  # Find 5, aka F
+
+print(Paths)
